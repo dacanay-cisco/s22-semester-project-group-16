f4bca0ac629a34376db134da939c3fe25c2f26a3
Cisco S Dacanay <cdacanay@localhost>
2022-04-21 12:46:50 -0700

    created processor and console class

---
+++ console.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+class Console {
+private:
+	Model model;
+
+public:
+	void fileRequest();
+	void printAttributes(Model model);
+};
+
+
+#endif //CONSOLE_H
